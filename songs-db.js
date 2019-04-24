7aa6aed2bfe239efe20176364332303c9822863e
Julian Duque <julianduquej@gmail.com>
2019-04-24 08:43:35 -0700

    Create SongsDb class

---
+++ songs-db.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const path = require('path')
+const SongModel = require('./models/song')
+
+class SongsDb {
+  constructor (dbFile) {
+    this._storage = path.join(__dirname, dbFile)
+    this._sequelize = null
+    this._song = null
+  }
+
+  async _init () {
+    this._sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: this._storage
+    })
+
+    const Song = this._sequelize.define('song', SongModel)
+
+    this._song = Song
+    return this._sequelize.sync()
+  }
+
+  static async at (dbFile = 'songs.db') {
+    const db = new SongsDb(dbFile)
+    await db._init()
+    return db
+  }
+}
+
+module.exports = SongsDb
